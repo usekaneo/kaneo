423539b71b12a8d03de1c9cd98768d010bb4acee
Ashish Mishra <anshm9448@gmail.com>
2025-04-16 20:19:05 +0530

    fix: fix workspace setting sidebar icon

--- apps/web/src/components/common/sidebar/sections/workspace-settings/index.tsx
+++ apps/web/src/components/common/sidebar/sections/workspace-settings/index.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/cn";
+import { useUserPreferencesStore } from "@/store/user-preferences";
 import useWorkspaceStore from "@/store/workspace";
 import { Link } from "@tanstack/react-router";
 import { useLocation } from "@tanstack/react-router";
@@ -7,7 +8,7 @@ import { Settings } from "lucide-react";
 function WorkspaceSettings() {
   const { workspace } = useWorkspaceStore();
   const location = useLocation();
-
+  const { isSidebarOpened } = useUserPreferencesStore();
   const isOnWorkspaceRoute = location.pathname.includes("/workspace-settings");
 
   if (!workspace) return null;
@@ -23,8 +24,13 @@ function WorkspaceSettings() {
           : "text-zinc-600 dark:text-zinc-400 hover:bg-zinc-100 dark:hover:bg-zinc-800 hover:text-zinc-900 dark:hover:text-zinc-100",
       )}
     >
-      <Settings className="w-4 h-4" />
-      <span>Workspace Settings</span>
+      <Settings
+        className={cn(
+          "shrink-0",
+          !isSidebarOpened ? "w-6 h-6" : "w-4 h-4 mr-2",
+        )}
+      />
+      {isSidebarOpened && "Workspace Settings"}
     </Link>
   );
 }
