88661a0bfae81b19fd157fc9d5b8871efd04b7de
Andrej <aacevski@gmail.com>
2025-04-20 17:53:53 +0200

    feat: set base path for documentation site to "/kaneo"

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  basePath: "/kaneo",
   output: "export",
   images: {
     unoptimized: true,
