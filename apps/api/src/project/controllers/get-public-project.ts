fafc9b666768709777e978ad1475570a30dc3f23
Andrej <aacevski@gmail.com>
2025-06-18 22:03:11 +0200

    refactor: replace Error with HTTPException

--- apps/api/src/project/controllers/get-public-project.ts
+++ apps/api/src/project/controllers/get-public-project.ts
@@ -1,14 +1,19 @@
+import { HTTPException } from "hono/http-exception";
 import getTasks from "../../task/controllers/get-tasks";
 
 export async function getPublicProject(id: string) {
   const project = await getTasks(id);
 
   if (!project) {
-    throw new Error("Project not found");
+    throw new HTTPException(404, {
+      message: "Project not found",
+    });
   }
 
   if (!project.isPublic) {
-    throw new Error("Project is not public");
+    throw new HTTPException(403, {
+      message: "Project is not public",
+    });
   }
 
   return project;
