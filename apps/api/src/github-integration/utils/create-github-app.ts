43ae891139d2974dacdf751fdedd723521efea50
Andrej Acevski <andrejacevski@pantone.com>
2025-06-23 10:29:18 +0200

    refactor: remove environment variable checks

--- apps/api/src/github-integration/utils/create-github-app.ts
+++ apps/api/src/github-integration/utils/create-github-app.ts
@@ -3,22 +3,11 @@ import { App } from "octokit";
 
 dotenv.config();
 
-if (
-  !process.env.GITHUB_APP_ID ||
-  !process.env.GITHUB_PRIVATE_KEY ||
-  !process.env.GITHUB_WEBHOOK_SECRET
-) {
-  throw new Error(
-    "GITHUB_APP_ID, GITHUB_PRIVATE_KEY and GITHUB_WEBHOOK_SECRET must be set",
-  );
-}
-
 const githubApp: App = new App({
-  appId: process.env.GITHUB_APP_ID,
-  privateKey: process.env.GITHUB_PRIVATE_KEY,
-
+  appId: process.env.GITHUB_APP_ID ?? "",
+  privateKey: process.env.GITHUB_PRIVATE_KEY ?? "",
   webhooks: {
-    secret: process.env.GITHUB_WEBHOOK_SECRET,
+    secret: process.env.GITHUB_WEBHOOK_SECRET ?? "",
   },
 });
 
