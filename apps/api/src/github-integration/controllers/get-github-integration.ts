619eacff7177b03f18d9a6d084ab20f76461379d
Andrej Acevski <aacevski@gmail.com>
2025-10-13 23:07:55 +0200

    chore: remove unused import from get-github-integration controller

--- apps/api/src/github-integration/controllers/get-github-integration.ts
+++ apps/api/src/github-integration/controllers/get-github-integration.ts
@@ -1,5 +1,4 @@
 import { eq } from "drizzle-orm";
-import { HTTPException } from "hono/http-exception";
 import db from "../../database";
 import { githubIntegrationTable } from "../../database/schema";
 
