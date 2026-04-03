b1a73d73fce66b70bb42991a3d358263572d0484
guillemc23 <guillemoreno.95@gmail.com>
2026-04-03 13:20:50 +0200

    fix: Sort imports

--- i18n/resources.ts
+++ i18n/resources.ts
@@ -1,19 +1,19 @@
 import deDE from "./de-DE.json";
 import elGR from "./el-GR.json";
 import enUS from "./en-US.json";
+import esES from "./es-ES.json";
 import frFR from "./fr-FR.json";
 import mkMK from "./mk-MK.json";
-import esES from "./es-ES.json";
 import nlNL from "./nl-NL.json";
 
 export const supportedLocales = [
   "mk-MK",
   "nl-NL",
-  "en-US",
   "de-DE",
   "el-GR",
-  "fr-FR",
+  "en-US",
   "es-ES",
+  "fr-FR",
 ] as const;
 
 export type AppLocale = (typeof supportedLocales)[number];
