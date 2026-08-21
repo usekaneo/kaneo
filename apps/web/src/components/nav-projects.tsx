2974f6c9de5d25328ffbed6cf2257d2aa28a1d9e
MonsPropre <b.baldy@proton.me>
2026-08-21 18:57:38 +0200

    fix: project nav width in different language

--- apps/web/src/components/nav-projects.tsx
+++ apps/web/src/components/nav-projects.tsx
@@ -278,7 +278,7 @@ export function NavProjects() {
                               </span>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent
-                              className="w-44 rounded-lg"
+                              className="w-auto rounded-lg"
                               side={isMobile ? "bottom" : "right"}
                               align={isMobile ? "end" : "start"}
                             >
