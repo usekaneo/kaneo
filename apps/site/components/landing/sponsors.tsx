97615910b2cdc4568b9cdde7df61078bd0e2d128
Taiyoung Jang <taiyoung.jang@gmail.com>
2026-08-26 23:54:42 +0900

    feat: improve hierarchy filters and attachment formatting

--- apps/site/components/landing/sponsors.tsx
+++ apps/site/components/landing/sponsors.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { FadeIn } from "@/components/landing/fade-in";
 import sponsors from "@/constants/sponsors.json";
+import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 type Sponsor = {
